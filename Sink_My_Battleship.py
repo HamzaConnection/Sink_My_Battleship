8d80a62fb450f771e584d06e5041c247ff82d76c
Hamza Laroussi <hamzaconnection@hotmail.com>
2017-11-09 12:55:02 +0100

    Simple sink my battleship done

---
+++ Sink_My_Battleship.py
@@ -0,0 +1,53 @@
+from random import randint
+
+board = []
+
+for x in range(0, 5):
+  board.append(["O"] * 5)
+
+def print_board(board):
+  for row in board:
+    print " ".join(row)
+
+print_board(board)
+
+def random_row(board):
+  return randint(0, len(board) - 1)
+
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+#For debugging purposes remember to remove
+print ship_row
+print ship_col
+
+
+for turn in range(4):
+
+  print "Turn", turn + 1
+  guess_row = (raw_input("Guess Row: "))
+  guess_col = (raw_input("Guess Col: "))
+
+  while guess_row.isdigit() == False or guess_col.isdigit() == False:
+        print "Please enter a number"
+        guess_row = raw_input("Guess Row: ")
+        guess_col = raw_input("Guess Col: ")
+
+  if guess_row == ship_row and guess_col == ship_col:
+    print "Congratulations! You sank my battleship!"
+    break
+  else:
+    if guess_row not in range(5) or \
+      guess_col not in range(5):
+      print "Oops, that's not even in the ocean."
+    elif board[guess_row][guess_col] == "X":
+      print( "You guessed that one already." )
+    else:
+      print "You missed my battleship!"
+      board[guess_row][guess_col] = "X"
+    print_board(board)
+    if turn == 3:
+      print 'Game Over'
+
